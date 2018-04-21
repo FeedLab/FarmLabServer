d7cb08cf9f8ce7cf54e2fbfbfa406d6b0066bea4
Unknown <jompa67@gmail.com>
2018-04-21 13:38:49 +0200

    .

---
+++ FarmLabService/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmLabService.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        //[Route("secret")]
+        //public IActionResult Secret()
+        //{
+        //    return View(new User(this.User));
+        //}
+
+        [Authorize]
+        [Route("home")]
+        public bool Home()
+        {
+            return User.Identities.Any(v => v.IsAuthenticated);
+        }
+    }
+}
